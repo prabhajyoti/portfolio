c4bfa37eae65407a230628746f61b8c9fed430c1
prabhajyoti <jyotiprabha81022@gmail.com>
2024-09-24 15:31:32 +0530

    new

--- app/src/main/java/com/jyoti/portfolio/Experience.kt
+++ app/src/main/java/com/jyoti/portfolio/Experience.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PreView : AppCompatActivity() {
+class Experience : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pre_view)
+        setContentView(R.layout.activity_experience)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
