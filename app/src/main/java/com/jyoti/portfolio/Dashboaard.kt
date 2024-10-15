f8b6baea0ff347e70fa52a31e9297819fa87b43e
prabhajyoti <jyotiprabha81022@gmail.com>
2024-10-15 20:22:46 +0530

    new

--- app/src/main/java/com/jyoti/portfolio/Dashboaard.kt
+++ app/src/main/java/com/jyoti/portfolio/Dashboaard.kt
@@ -1,6 +1,8 @@
 package com.jyoti.portfolio
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class Dashboaard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboaard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val enter = findViewById<Button>(R.id.enter)
+
+        enter.setOnClickListener { this
+        val Intent = Intent(this, About::class.java)
+        startActivity(Intent)
         }
     }
 }
