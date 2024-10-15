6a4f144e6bc611bfa422984bda4ad6ebe90f6f41
prabhajyoti <jyotiprabha81022@gmail.com>
2024-10-15 20:47:52 +0530

    new

--- app/src/main/java/com/jyoti/portfolio/About.kt
+++ app/src/main/java/com/jyoti/portfolio/About.kt
@@ -11,10 +11,7 @@ class About : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_about)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        vale
     }
 }
