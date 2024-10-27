13be2e296e30cc20cc290710f9d02637fa866d10
prabhajyoti <jyotiprabha81022@gmail.com>
2024-10-27 17:09:24 +0530

    new

--- app/src/main/java/com/jyoti/portfolio/About.kt
+++ app/src/main/java/com/jyoti/portfolio/About.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -13,12 +14,32 @@ class About : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_about)
-
         val experience = findViewById<Button>(R.id.experience)
         experience.setOnClickListener { this
-        val Intent = Intent(this,experience::class.java)
-        startActivity(Intent)
+            val Intent = Intent(this, Experience::class.java)
+            startActivity(Intent)
         }
+
+
+
+        setupLinkButton()
     }
+       fun setupLinkButton() {
+           val cvlinkButton = findViewById<Button>(R.id.cv);
+           cvlinkButton.setOnClickListener { this
+              val browserIntent = Intent(
+                  Intent.ACTION_VIEW,
+                  Uri.parse("https://drive.google.com/file/d/1iNroc-4XhvCwkVI0qY_4oeE_SgWJavW0/view?usp=sharing")
+              )
+                startActivity(browserIntent)
+
+           }
+
+
+       }
+
+
+
+
     
 }
